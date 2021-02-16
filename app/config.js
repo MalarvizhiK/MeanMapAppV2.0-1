64e7eaa83b37fcce54ec7df6daee8620ec6ce67f
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2021-02-16 12:07:30 +0530

    Changes for docker

--- app/config.js
+++ app/config.js
@@ -27,6 +27,8 @@ module.exports = {
     {
         name: "MongoDB Service",
         url: "mongodb://" + process.env.DATABASE_USER + ":" + process.env.DATABASE_PASSWORD + "@" + process.env.DATABASE_HOST + "/" + process.env.DATABASE_NAME,
+        // url : "mongodb://localhost:27017/mydb",
+	// port: 27017
         port: process.env.DATABASE_PORT
     }
 
